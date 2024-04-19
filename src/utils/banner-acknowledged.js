34d3de5e5c7af9e77c3fa6564120cc95c73d762d
Phil Hawksworth <phil@netlify.com>
2024-04-19 16:10:53 +0100

    html check mark

--- src/utils/banner-acknowledged.js
+++ src/utils/banner-acknowledged.js
@@ -1,7 +1,7 @@
 import container from '../utils/banner-container.js';
 
 export default () => {
-  const content = `<a href="?cookieBannerAction=revert">Cookies</a> `;
+  const content = `<a href="?cookieBannerAction=revert">&#10003;</a> `;
   return container(content);
 }
 
